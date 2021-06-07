9b5c0dda5d52ef1fed585a373c9efab4c4d3c36d
Jason <hoangxl@hitachiconsulting.com>
2021-06-07 14:32:28 +0800

    Minor changes

--- src/web3.js
+++ src/web3.js
@@ -1,6 +1,8 @@
 import Web3 from 'web3';
 
 const web3 = new Web3(window.web3.currentProvider);
-//If error then change to window.web3.currentProvider.enable()
+//If receive error then change the above statement to 
+//const web3 = new Web3(window.web3.currentProvider.enable());
+//After changing the statement and do npm start, change the statement back to the original and npm start again
 
 export default web3;
